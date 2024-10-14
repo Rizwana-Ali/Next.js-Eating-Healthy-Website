b7f1574e435198f28739bfb9770ef7f0b3c34aae
Rizwana Ali <160421362+Rizwana-Ali@users.noreply.github.com>
2024-10-14 10:33:58 -0700

    Add files via upload

---
+++ page.tsx
@@ -0,0 +1,10 @@
+function Homepage () {
+    let age:number = 18
+    return ( <div><h1><b><u>Hello World {age}</u></b></h1>
+       <h2>Alhamdulillah! This is my first Hello World of next js journey using variable that I have done successfully.</h2>
+</div>
+  )}
+  
+  export default Homepage
+
+  
